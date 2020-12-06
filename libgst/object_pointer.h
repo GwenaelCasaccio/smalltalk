f1185ee8c4dbebe0bfd5bf407b22b724b12ba688
Gwenaël Casaccio <gwenael.casaccio@qima.com>
2020-12-06 20:30:14 +0100

    refactor gst_method_context structwq

--- libgst/object_pointer.h
+++ libgst/object_pointer.h
@@ -6,9 +6,22 @@
 #define OBJ_INDEXED_OBJECT(obj, n)    ( ((OOP *) ((obj) + 1))[(n)-1] )
 #define OBJ_ARRAY_AT(obj, n)   ( ((OOP *) ((gst_object) obj)->data)[(n)-1] )
 #define OBJ_STRING_AT(obj, n)  ( ((char *) ((gst_object) obj)->data)[(n)-1] )
+
 #define OBJ_CLASS(obj) ((obj)->objClass)
+
 #define OBJ_SET_CLASS(obj, valueOOP) do { \
   (obj)->objClass = (valueOOP);           \
 } while (0)
+
+/* METHOD CONTEXT */
+
+#define OBJ_METHOD_CONTEXT_PARENT_CONTEXT(obj) ((obj)->data[0])
+#define OBJ_METHOD_CONTEXT_NATIVE_IP(obj) ((obj)->data[1])
+#define OBJ_METHOD_CONTEXT_IP_OFFSET(obj) ((obj)->data[2])
+#define OBJ_METHOD_CONTEXT_SP_OFFSET(obj) ((obj)->data[3])
+#define OBJ_METHOD_CONTEXT_RECEIVER(obj) ((obj)->data[4])
+#define OBJ_METHOD_CONTEXT_METHOD(obj) ((obj)->data[5])
+#define OBJ_METHOD_CONTEXT_FLAGS(obj) ((obj)->data[6])
+#define OBJ_METHOD_CONTEXT_CONTEXT_STACK(obj) (&(obj)->data[7])
 
 #endif /* GST_OBJECT_POINTER_H */
