a6cd76b20b210875351d2d59b10e96dcdb9f5a86
Gwenaël Casaccio <gwenael.casaccio@gmail.com>
2021-03-09 14:04:40 +0100

    feat: test alloc_oop

--- tests/vm/forward_object.c
+++ tests/vm/forward_object.c
@@ -18,6 +18,14 @@ void __wrap__gst_display_oop(OOP oop);
 
 void __wrap__gst_display_oop_short(OOP oop);
 
+int __wrap_pthread_mutex_lock(pthread_mutex_t *mutex);
+
+int __wrap_pthread_mutex_unlock(pthread_mutex_t *mutex);
+
+void __wrap__gst_finished_incremental_gc(void);
+
+void __wrap__gst_sweep_oop(OOP oop);
+
 static void null_test_success(void **state) {
   (void) state;
 }
@@ -52,6 +60,26 @@ void __wrap__gst_display_oop(OOP oop) {
 void __wrap__gst_display_oop_short(OOP oop) {
   abort();
   return ;
+}
+
+int __wrap_pthread_mutex_lock(pthread_mutex_t *mutex) {
+  check_expected(mutex);
+
+  return mock_type(int);
+}
+
+int __wrap_pthread_mutex_unlock(pthread_mutex_t *mutex) {
+  check_expected(mutex);
+
+  return mock_type(int);
+}
+
+void __wrap__gst_finished_incremental_gc(void) {
+
+}
+
+void __wrap__gst_sweep_oop(OOP oop) {
+  check_expected(oop);
 }
 
 static void init_oop_table_with_null_heap(void **state) {
@@ -202,6 +230,91 @@ static void realloc_oop_table_sbrk_fail(void **state) {
   assert_true(_gst_mem.ot_size == 0x1000);
 }
 
+static void test_alloc_oop(void **state) {
+  OOP result;
+
+  (void) state;
+
+  _gst_mem.ot = calloc(1000, sizeof(struct oop_s));
+  if (!_gst_mem.ot) { abort(); }
+
+  _gst_mem.num_free_oops = 1000;
+  _gst_mem.ot_size = 1000;
+  _gst_mem.next_oop_to_sweep = _gst_mem.last_allocated_oop = _gst_mem.last_swept_oop = _gst_mem.ot - 1;
+  _gst_mem.live_flags = 0x2;
+
+  expect_value(__wrap_pthread_mutex_lock, mutex, &alloc_oop_mutex);
+  will_return(__wrap_pthread_mutex_lock, 0);
+
+  expect_value(__wrap_pthread_mutex_unlock, mutex, &alloc_oop_mutex);
+  will_return(__wrap_pthread_mutex_unlock, 0);
+
+  result = alloc_oop((PTR) 0xBABA, 0x1234);
+
+  assert_true(result == _gst_mem.ot);
+  assert_true(OOP_GET_FLAGS(result) == 0x1234);
+  assert_true(OOP_TO_OBJ(result) == (gst_object) 0xBABA);
+  assert_true(_gst_mem.last_swept_oop == result);
+  assert_true(_gst_mem.num_free_oops == 999);
+  assert_true(_gst_mem.last_allocated_oop == result);
+
+  /* Skip first allocated and test if the others are still free */
+  for (size_t i = 1; i < 1000; i++) {
+    assert_true(OOP_GET_FLAGS(&_gst_mem.ot[i]) == 0);
+  }
+
+  free(_gst_mem.ot);
+}
+
+static void test_alloc_oop_with_allocated_objects(void **state) {
+  OOP result;
+
+  (void) state;
+
+  _gst_mem.live_flags = 0x2;
+
+  _gst_mem.ot = calloc(1000, sizeof(struct oop_s));
+  if (!_gst_mem.ot) { abort(); }
+
+  /* Skip first allocated and test if the others are still free */
+  for (size_t i = 0; i < 500; i++) {
+    OOP_SET_FLAGS(&_gst_mem.ot[i], _gst_mem.live_flags);
+  }
+
+  _gst_mem.num_free_oops = 500;
+  _gst_mem.ot_size = 1000;
+  _gst_mem.last_allocated_oop = &_gst_mem.ot[499];
+  _gst_mem.next_oop_to_sweep = _gst_mem.last_swept_oop = _gst_mem.ot - 1;
+
+  expect_value(__wrap_pthread_mutex_lock, mutex, &alloc_oop_mutex);
+  will_return(__wrap_pthread_mutex_lock, 0);
+
+  expect_value(__wrap_pthread_mutex_unlock, mutex, &alloc_oop_mutex);
+  will_return(__wrap_pthread_mutex_unlock, 0);
+
+  result = alloc_oop((PTR) 0xBABA, 0x1234);
+
+  assert_true(result == &_gst_mem.ot[500]);
+  assert_true(OOP_GET_FLAGS(result) == 0x1234);
+  assert_true(OOP_TO_OBJ(result) == (gst_object) 0xBABA);
+  assert_true(_gst_mem.last_swept_oop == result);
+  assert_true(_gst_mem.num_free_oops == 499);
+  assert_true(_gst_mem.last_allocated_oop == result);
+
+  /* Skip firsts allocated and test if the others are still free */
+  for (size_t i = 1; i < 500; i++) {
+    assert_true(OOP_GET_FLAGS(&_gst_mem.ot[i]) == _gst_mem.live_flags);
+  }
+
+  assert_true(OOP_GET_FLAGS(&_gst_mem.ot[500]) == 0x1234);
+
+  for (size_t i = 501; i < 1000; i++) {
+    assert_true(OOP_GET_FLAGS(&_gst_mem.ot[i]) == 0);
+  }
+
+  free(_gst_mem.ot);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] =
     {
@@ -213,6 +326,8 @@ int main(void) {
      cmocka_unit_test(realloc_oop_table),
      cmocka_unit_test(realloc_oop_table_with_smaller_new_number_of_oop),
      cmocka_unit_test(realloc_oop_table_sbrk_fail),
+     cmocka_unit_test(test_alloc_oop),
+     cmocka_unit_test(test_alloc_oop_with_allocated_objects),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
