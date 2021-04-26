149b247d7c5af9eb8ca877886febd50bf19db4c6
Gwenaël Casaccio <gwenael.casaccio@gmail.com>
2021-04-26 13:53:36 +0200

    feat: more tests for heap

--- tests/vm/heap_test.c
+++ tests/vm/heap_test.c
@@ -91,7 +91,6 @@ static void should_allocate_heap(void **state) {
 
   (void) state;
 
-
   expect_value(__wrap_posix_memalign, alignment, 0x200000);
   expect_value(__wrap_posix_memalign, size, sizeof(gst_heap_t));
   will_return(__wrap_posix_memalign, 0);
@@ -122,6 +121,61 @@ static void should_allocate_heap(void **state) {
   free(next_heap_ptr);
 }
 
+static void should_align_heap_size(void **state) {
+  gst_heap_t *heap_ptr = NULL;
+  gst_heap_t *next_heap_ptr = NULL;
+
+  (void) state;
+
+  expect_value(__wrap_posix_memalign, alignment, 0x200000);
+  expect_value(__wrap_posix_memalign, size, sizeof(gst_heap_t));
+  will_return(__wrap_posix_memalign, 0);
+
+  expect_value(__wrap_posix_memalign, alignment, 0x200000);
+  expect_value(__wrap_posix_memalign, size, sizeof(gst_heap_t));
+  will_return(__wrap_posix_memalign, 0);
+
+  expect_function_calls(__wrap_posix_memalign, 2);
+
+  _gst_heap_new_area(&heap_ptr, 0x234000);
+
+  assert_true(NULL != heap_ptr);
+
+  assert_true(heap_ptr->meta_inf.free_space == sizeof(gst_heap_t) - sizeof(gst_heap_meta_inf_t));
+  assert_true(heap_ptr->meta_inf.prev_heap_area == NULL);
+  assert_true(heap_ptr->meta_inf.next_heap_area != NULL);
+  assert_true(heap_ptr->meta_inf.reserved_for_allocator == NULL);
+
+  next_heap_ptr = heap_ptr->meta_inf.next_heap_area;
+
+  assert_true(next_heap_ptr->meta_inf.free_space == sizeof(gst_heap_t) - sizeof(gst_heap_meta_inf_t));
+  assert_true(next_heap_ptr->meta_inf.prev_heap_area == heap_ptr);
+  assert_true(next_heap_ptr->meta_inf.next_heap_area == NULL);
+  assert_true(next_heap_ptr->meta_inf.reserved_for_allocator == NULL);
+
+  free(heap_ptr);
+  free(next_heap_ptr);
+}
+
+static void should_free_heap_if_null(void **state) {
+  gst_heap_t *heap_ptr = NULL;
+
+  (void) state;
+
+  heap_ptr = malloc(sizeof(*heap_ptr));
+  heap_ptr->meta_inf.next_heap_area = malloc(sizeof(*heap_ptr));
+
+ _gst_heap_free_area(heap_ptr);
+}
+
+static void should_free_heap(void **state) {
+  gst_heap_t *heap_ptr = NULL;
+
+  (void) state;
+
+  _gst_heap_free_area(heap_ptr);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] =
     {
@@ -129,6 +183,9 @@ int main(void) {
      cmocka_unit_test(should_failed_when_heap_address_is_not_null),
      cmocka_unit_test(should_failed_when_posix_memalign_failed),
      cmocka_unit_test(should_allocate_heap),
+     cmocka_unit_test(should_align_heap_size),
+     cmocka_unit_test(should_free_heap_if_null),
+     cmocka_unit_test(should_free_heap),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
