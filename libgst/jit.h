76114e4f66e7c85e8a4149266e631edd42db4362
Gwenaël Casaccio <gwenael.casaccio@gmail.com>
2021-06-07 08:32:53 +0200

    chore: WIP jit compilation

---
+++ libgst/jit.h
@@ -0,0 +1,6 @@
+#ifndef GST_JIT_H
+#define GST_JIT_H
+
+void *compile(OOP compiled_code_oop);
+
+#endif /* GST_JIT_H */
--- tests/vm/jit_x64.c
+++ tests/vm/jit_x64.c
@@ -1,4 +1,17 @@
 #include <unicorn/unicorn.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "libgst/jit-x64.c"
+
+OOP _gst_nil_oop;
+OOP _gst_true_oop;
+OOP _gst_false_oop;
+OOP _gst_compiled_method_class;
+OOP _gst_compiled_block_class;
 
 int main(int argc, const char **argv) {
   return 0;
