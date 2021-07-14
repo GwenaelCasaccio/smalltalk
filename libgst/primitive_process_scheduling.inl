24b631e51a7034319dacaa9b2c26c8e658272ae7
Gwenaël Casaccio <gwenael.casaccio@gmail.com>
2021-07-14 10:34:07 +0200

    feat: add more tests

--- libgst/primitive_process_scheduling.inl
+++ libgst/primitive_process_scheduling.inl
@@ -39,15 +39,36 @@ static intptr_t VMpr_Process_resume(int id, volatile int numArgs) {
   }
 }
 
-/* Process singleStepWaitingOn: */
-static intptr_t VMpr_Process_singleStepWaitingOn(int id, volatile int numArgs) {
-  OOP oop1;
-  OOP oop2;
-
+/* Process yield */
+static intptr_t VMpr_Process_yield(int id, volatile int numArgs) {
   _gst_primitives_executed++;
 
-  oop2 = POP_OOP();
-  oop1 = POP_OOP();
+  const OOP oop1 = STACKTOP();
+  const OOP processorSchedulerOOP = OBJ_PROCESS_GET_PROCESSOR_SCHEDULER(OOP_TO_OBJ(oop1));
+
+  if (!IS_NIL(processorSchedulerOOP) && processorSchedulerOOP != _gst_processor_oop[current_thread_id]) {
+    PRIM_FAILED;
+  }
+
+  if (oop1 == get_active_process()) {
+    SET_STACKTOP(_gst_nil_oop); /* this is our return value */
+    active_process_yield();
+  }
+  PRIM_SUCCEEDED;
+}
+
+/* Process singleStepWaitingOn: */
+static intptr_t VMpr_Process_singleStepWaitingOn(int id, volatile int numArgs) {
+  _gst_primitives_executed++;
+
+  const OOP oop2 = POP_OOP();
+  const OOP oop1 = POP_OOP();
+  const OOP processorSchedulerOOP = OBJ_PROCESS_GET_PROCESSOR_SCHEDULER(OOP_TO_OBJ(oop1));
+
+  if (!IS_NIL(processorSchedulerOOP) && processorSchedulerOOP != _gst_processor_oop[current_thread_id]) {
+    UNPOP(2);
+    PRIM_FAILED;
+  }
 
   if (is_process_ready(oop1) || is_process_terminating(oop1)) {
     UNPOP(2);
@@ -60,19 +81,6 @@ static intptr_t VMpr_Process_singleStepWaitingOn(int id, volatile int numArgs) {
   _gst_sync_wait(oop2);
   resume_process(oop1, true);
   single_step_semaphore = oop2;
-  PRIM_SUCCEEDED;
-}
-
-/* Process yield */
-static intptr_t VMpr_Process_yield(int id, volatile int numArgs) {
-  OOP oop1;
-
-  _gst_primitives_executed++;
-  oop1 = STACKTOP();
-  if (oop1 == get_active_process()) {
-    SET_STACKTOP(_gst_nil_oop); /* this is our return value */
-    active_process_yield();
-  }
   PRIM_SUCCEEDED;
 }
 
--- tests/vm/primitive_process.c
+++ tests/vm/primitive_process.c
@@ -40,13 +40,17 @@ bool __wrap_resume_process(OOP processOOP, bool resume) {
   return (bool) mock();
 }
 
-bool __wrap_is_process_ready(OOP process) {
+bool __wrap_is_process_ready(OOP processOOP) {
+  check_expected(processOOP);
+
   function_called();
 
   return (bool) mock();
 }
 
-bool __wrap_is_process_terminating(OOP process) {
+bool __wrap_is_process_terminating(OOP processOOP) {
+  check_expected(processOOP);
+
   function_called();
 
   return (bool) mock();
@@ -62,7 +66,9 @@ void __wrap_active_process_yield() {
   function_called();
 }
 
-void _gst_sync_wait(OOP semaphore) {
+void __wrap__gst_sync_wait(OOP semaphoreOOP) {
+  check_expected(semaphoreOOP);
+
   function_called();
 }
 
@@ -182,6 +188,197 @@ static void should_resume_process_and_set_process_scheduler(void **state) {
   assert_true(OBJ_PROCESS_GET_PROCESSOR_SCHEDULER(process) == _gst_processor_oop[current_thread_id]);
 }
 
+static void should_not_yield_process_when_scheduler_is_not_the_thread_one(void **state) {
+
+  (void) state;
+
+  // When
+  OOP processorOOP = malloc(sizeof(*processorOOP));
+  OOP processOOP = malloc(sizeof(*processOOP));
+  gst_object process = calloc(100, sizeof(OOP));
+  OOP_SET_OBJECT(processOOP, process);
+  OBJ_PROCESS_SET_PROCESSOR_SCHEDULER(process, processorOOP);
+
+  current_thread_id = 0;
+  _gst_processor_oop[current_thread_id] = (OOP) 0x1234;
+  sp[current_thread_id] = &processOOP;
+
+  // Then
+  intptr_t result = VMpr_Process_yield(123, 0);
+
+  assert_true(result == true);
+}
+
+static void should_not_yield_process_when_process_is_not_active(void **state) {
+
+  (void) state;
+
+  // When
+  OOP processOOP = malloc(sizeof(*processOOP));
+  gst_object process = calloc(100, sizeof(OOP));
+  OOP_SET_OBJECT(processOOP, process);
+  OBJ_PROCESS_SET_PROCESSOR_SCHEDULER(process, (OOP) 0x1234);
+
+  current_thread_id = 0;
+  _gst_processor_oop[current_thread_id] = (OOP) 0x1234;
+  sp[current_thread_id] = &processOOP;
+
+  // Then
+  will_return(__wrap_get_active_process, NULL);
+  expect_function_calls(__wrap_get_active_process, 1);
+
+  intptr_t result = VMpr_Process_yield(123, 0);
+
+  assert_true(result == false);
+}
+
+static void should_not_yield_process(void **state) {
+
+  (void) state;
+
+  // When
+  OOP processOOP = malloc(sizeof(*processOOP));
+  gst_object process = calloc(100, sizeof(OOP));
+  OOP_SET_OBJECT(processOOP, process);
+  OBJ_PROCESS_SET_PROCESSOR_SCHEDULER(process, (OOP) 0x1234);
+
+  current_thread_id = 0;
+  _gst_processor_oop[current_thread_id] = (OOP) 0x1234;
+  sp[current_thread_id] = &processOOP;
+
+  // Then
+  will_return(__wrap_get_active_process, processOOP);
+  expect_function_calls(__wrap_get_active_process, 1);
+
+  expect_function_calls(__wrap_active_process_yield, 1);
+
+  intptr_t result = VMpr_Process_yield(123, 0);
+
+  assert_true(result == false);
+}
+
+static void should_not_step_process_when_scheduler_is_not_the_thread_one(void **state) {
+
+  (void) state;
+
+  // When
+  OOP semaphoreOOP = malloc(sizeof(*semaphoreOOP));
+  OOP processorOOP = malloc(sizeof(*processorOOP));
+  OOP processOOP = malloc(sizeof(*processOOP));
+  gst_object process = calloc(100, sizeof(OOP));
+  OOP_SET_OBJECT(processOOP, process);
+  OBJ_PROCESS_SET_PROCESSOR_SCHEDULER(process, processorOOP);
+
+  current_thread_id = 0;
+  _gst_processor_oop[current_thread_id] = (OOP) 0x1234;
+  OOP stack[2] = { processOOP, semaphoreOOP };
+  sp[current_thread_id] = &stack[1];
+
+  // Then
+  intptr_t result = VMpr_Process_singleStepWaitingOn(123, 0);
+
+  assert_true(result == true);
+  assert_true(sp[current_thread_id] == &stack[1]);
+}
+
+static void should_not_step_process_when_process_is_not_ready(void **state) {
+
+  (void) state;
+
+  // When
+  OOP semaphoreOOP = malloc(sizeof(*semaphoreOOP));
+  OOP processOOP = malloc(sizeof(*processOOP));
+  gst_object process = calloc(100, sizeof(OOP));
+  OOP_SET_OBJECT(processOOP, process);
+  OBJ_PROCESS_SET_PROCESSOR_SCHEDULER(process, (OOP) 0x1234);
+
+  current_thread_id = 0;
+  _gst_processor_oop[current_thread_id] = (OOP) 0x1234;
+  OOP stack[2] = { processOOP, semaphoreOOP };
+  sp[current_thread_id] = &stack[1];
+
+  // Then
+  expect_value(__wrap_is_process_ready, processOOP, processOOP);
+  will_return(__wrap_is_process_ready, true);
+  expect_function_calls(__wrap_is_process_ready, 1);
+
+  intptr_t result = VMpr_Process_singleStepWaitingOn(123, 0);
+
+  assert_true(result == true);
+  assert_true(sp[current_thread_id] == &stack[1]);
+}
+
+static void should_not_step_process_when_process_is_terminating(void **state) {
+
+  (void) state;
+
+  // When
+  OOP semaphoreOOP = malloc(sizeof(*semaphoreOOP));
+  OOP processOOP = malloc(sizeof(*processOOP));
+  gst_object process = calloc(100, sizeof(OOP));
+  OOP_SET_OBJECT(processOOP, process);
+  OBJ_PROCESS_SET_PROCESSOR_SCHEDULER(process, (OOP) 0x1234);
+
+  current_thread_id = 0;
+  _gst_processor_oop[current_thread_id] = (OOP) 0x1234;
+  OOP stack[2] = { processOOP, semaphoreOOP };
+  sp[current_thread_id] = &stack[1];
+
+  // Then
+  expect_value(__wrap_is_process_ready, processOOP, processOOP);
+  will_return(__wrap_is_process_ready, false);
+  expect_function_calls(__wrap_is_process_ready, 1);
+
+  expect_value(__wrap_is_process_terminating, processOOP, processOOP);
+  will_return(__wrap_is_process_terminating, true);
+  expect_function_calls(__wrap_is_process_terminating, 1);
+
+  intptr_t result = VMpr_Process_singleStepWaitingOn(123, 0);
+
+  assert_true(result == true);
+  assert_true(sp[current_thread_id] == &stack[1]);
+}
+
+static void should_step_process(void **state) {
+
+  (void) state;
+
+  // When
+  OOP semaphoreOOP = malloc(sizeof(*semaphoreOOP));
+  OOP processOOP = malloc(sizeof(*processOOP));
+  gst_object process = calloc(100, sizeof(OOP));
+  OOP_SET_OBJECT(processOOP, process);
+  OBJ_PROCESS_SET_PROCESSOR_SCHEDULER(process, (OOP) 0x1234);
+
+  current_thread_id = 0;
+  _gst_processor_oop[current_thread_id] = (OOP) 0x1234;
+  OOP stack[3] = { NULL, processOOP, semaphoreOOP };
+  sp[current_thread_id] = &stack[2];
+
+  // Then
+  expect_value(__wrap_is_process_ready, processOOP, processOOP);
+  will_return(__wrap_is_process_ready, false);
+  expect_function_calls(__wrap_is_process_ready, 1);
+
+  expect_value(__wrap_is_process_terminating, processOOP, processOOP);
+  will_return(__wrap_is_process_terminating, false);
+  expect_function_calls(__wrap_is_process_terminating, 1);
+
+  expect_value(__wrap__gst_sync_wait, semaphoreOOP, semaphoreOOP);
+  expect_function_calls(__wrap__gst_sync_wait, 1);
+
+  expect_value(__wrap_resume_process, processOOP, processOOP);
+  expect_value(__wrap_resume_process, resume, true);
+  will_return(__wrap_resume_process, true);
+  expect_function_calls(__wrap_resume_process, 1);
+
+  intptr_t result = VMpr_Process_singleStepWaitingOn(123, 0);
+
+  assert_true(result == false);
+  assert_true(sp[current_thread_id] == &stack[0]);
+  assert_true(single_step_semaphore == semaphoreOOP);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] =
     {
@@ -190,6 +387,13 @@ int main(void) {
       cmocka_unit_test(should_not_resume_process_when_scheduler_is_not_the_thread_one),
       cmocka_unit_test(should_resume_process),
       cmocka_unit_test(should_resume_process_and_set_process_scheduler),
+      cmocka_unit_test(should_not_yield_process_when_scheduler_is_not_the_thread_one),
+      cmocka_unit_test(should_not_yield_process_when_process_is_not_active),
+      cmocka_unit_test(should_not_yield_process),
+      cmocka_unit_test(should_not_step_process_when_scheduler_is_not_the_thread_one),
+      cmocka_unit_test(should_not_step_process_when_process_is_not_ready),
+      cmocka_unit_test(should_not_step_process_when_process_is_terminating),
+      cmocka_unit_test(should_step_process),
     };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
