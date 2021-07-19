c99818eb4ddfe4c1b3863898a3deca286a4488e4
Gwenael Casaccio <gwenael.casaccio@gmail.com>
2021-07-19 13:13:39 +0200

    feat: introduces atomic operation and use them in semaphore signals (#81)
    * feat: introduces atomic operation and use them in semaphore signals
    
    * test semaphore getter and setter signals
    
    * feat: introduces atomic operation and use them in semaphore signals

--- tests/vm/object_pointer.c
+++ tests/vm/object_pointer.c
@@ -19,10 +19,23 @@ static void should_get_and_set_vm_thread_id(void **state) {
   assert_true(TO_INT(OBJ_PROCESSOR_SCHEDULER_GET_VM_THREAD_ID(processor)) == 123);
 }
 
+static void should_get_and_set_semaphore_signals(void **state) {
+
+  (void) state;
+
+  /* When */
+  gst_object semaphore = calloc(20, sizeof(OOP));
+  OBJ_SEMAPHORE_SET_SIGNALS(semaphore, FROM_INT(123));
+
+  /* Then */
+  assert_true(TO_INT(OBJ_SEMAPHORE_GET_SIGNALS(semaphore)) == 123);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] =
     {
       cmocka_unit_test(should_get_and_set_vm_thread_id),
+      cmocka_unit_test(should_get_and_set_semaphore_signals),
     };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
