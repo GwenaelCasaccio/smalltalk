03c8ec27ef3f86a1820af5d0f561acd5d75af0ca
Gwenael Casacio <gwenael.casaccio@gmail.com>
2022-02-28 21:52:44 +0100

    feat: move to outer register

--- tests/vm/vm.c
+++ tests/vm/vm.c
@@ -327,6 +327,108 @@ static void should_move_ivar_to_register(void **state) {
   free(context);
 }
 
+static void should_move_register_to_outer_register(void **state) {
+
+  (void) state;
+
+  uint32_t bytecode[] = { 0x0C, 0x2, 0x2, 0x1, 0xFF };
+  tip = &bytecode[0];
+  OOP literals[]  = { NULL, NULL, NULL };
+  _gst_literals[0] = &literals[0];
+  _gst_this_context_oop[0] = malloc(sizeof(*_gst_this_context_oop[0]));
+  context = malloc(sizeof(*context) * 100);
+  context->data[0x2] = FROM_INT(123);
+  OOP_SET_OBJECT(_gst_this_context_oop[0], context);
+  OOP outer_context_1_oop = malloc(sizeof(*outer_context_1_oop));
+  gst_object outer_context_1 = malloc(sizeof(*outer_context_1) * 100);
+  OOP_SET_OBJECT(outer_context_1_oop, outer_context_1);
+  OOP outer_context_2_oop = malloc(sizeof(*outer_context_2_oop));
+  gst_object outer_context_2 = malloc(sizeof(*outer_context_2) * 100);
+  OOP_SET_OBJECT(outer_context_2_oop, outer_context_2);
+  OBJ_BLOCK_CONTEXT_SET_OUTER_CONTEXT(context, outer_context_1_oop);
+  OBJ_BLOCK_CONTEXT_SET_OUTER_CONTEXT(outer_context_1, outer_context_2_oop);
+  
+  bc();
+  
+  assert_true(tip == &bytecode[5]);
+  assert_true(outer_context_2->data[0x1] == FROM_INT(123));
+
+  free(context);
+  free(outer_context_1_oop);
+  free(outer_context_1);
+  free(outer_context_2_oop);
+  free(outer_context_2);
+}
+
+static void should_move_outer_register_to_outer_register(void **state) {
+
+  (void) state;
+
+  uint32_t bytecode[] = { 0x0D, 0x1, 0x2, 0x2, 0x3, 0xFF };
+  tip = &bytecode[0];
+  OOP literals[]  = { NULL, NULL, NULL };
+  _gst_literals[0] = &literals[0];
+  _gst_this_context_oop[0] = malloc(sizeof(*_gst_this_context_oop[0]));
+  context = malloc(sizeof(*context) * 100);
+  OOP_SET_OBJECT(_gst_this_context_oop[0], context);
+  OOP outer_context_1_oop = malloc(sizeof(*outer_context_1_oop));
+  gst_object outer_context_1 = malloc(sizeof(*outer_context_1) * 100);
+  OOP_SET_OBJECT(outer_context_1_oop, outer_context_1);
+  outer_context_1->data[0x2] = FROM_INT(123);
+  OOP outer_context_2_oop = malloc(sizeof(*outer_context_2_oop));
+  gst_object outer_context_2 = malloc(sizeof(*outer_context_2) * 100);
+  OOP_SET_OBJECT(outer_context_2_oop, outer_context_2);
+  OBJ_BLOCK_CONTEXT_SET_OUTER_CONTEXT(context, outer_context_1_oop);
+  OBJ_BLOCK_CONTEXT_SET_OUTER_CONTEXT(outer_context_1, outer_context_2_oop);
+  
+  bc();
+  
+  assert_true(tip == &bytecode[6]);
+  assert_true(outer_context_2->data[0x3] == FROM_INT(123));
+
+  free(context);
+  free(outer_context_1_oop);
+  free(outer_context_1);
+  free(outer_context_2_oop);
+  free(outer_context_2);
+}
+
+static void should_move_ivar_to_outer_register(void **state) {
+
+  (void) state;
+
+  uint32_t bytecode[] = { 0x0E, 0x2, 0x2, 0x1, 0xFF };
+  tip = &bytecode[0];
+  OOP literals[]  = { NULL, NULL, NULL };
+  _gst_literals[0] = &literals[0];
+  _gst_this_context_oop[0] = malloc(sizeof(*_gst_this_context_oop[0]));
+  context = malloc(sizeof(*context) * 100);
+  OOP_SET_OBJECT(_gst_this_context_oop[0], context);
+  OOP outer_context_1_oop = malloc(sizeof(*outer_context_1_oop));
+  gst_object outer_context_1 = malloc(sizeof(*outer_context_1) * 100);
+  OOP_SET_OBJECT(outer_context_1_oop, outer_context_1);
+  OOP outer_context_2_oop = malloc(sizeof(*outer_context_2_oop));
+  gst_object outer_context_2 = malloc(sizeof(*outer_context_2) * 100);
+  OOP_SET_OBJECT(outer_context_2_oop, outer_context_2);
+  OBJ_BLOCK_CONTEXT_SET_OUTER_CONTEXT(context, outer_context_1_oop);
+  OBJ_BLOCK_CONTEXT_SET_OUTER_CONTEXT(outer_context_1, outer_context_2_oop);
+  _gst_self[0] = malloc(sizeof(*_gst_self[0]));
+  gst_object obj = malloc(sizeof(*context) * 100);
+  OOP_SET_OBJECT(_gst_self[0], obj);
+  obj->data[0x02] = FROM_INT(123);
+  
+  bc();
+  
+  assert_true(tip == &bytecode[5]);
+  assert_true(outer_context_2->data[0x1] == FROM_INT(123));
+
+  free(context);
+  free(outer_context_1_oop);
+  free(outer_context_1);
+  free(outer_context_2_oop);
+  free(outer_context_2);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] =
     {
@@ -343,6 +445,9 @@ int main(void) {
       cmocka_unit_test(should_move_register_to_register),
       cmocka_unit_test(should_move_outer_scope_to_register),
       cmocka_unit_test(should_move_ivar_to_register),
+      cmocka_unit_test(should_move_register_to_outer_register),
+      cmocka_unit_test(should_move_outer_register_to_outer_register),
+      cmocka_unit_test(should_move_ivar_to_outer_register),
     };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
