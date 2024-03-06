158df20903ecb4a8f5983ccb267d163e6d320df1
Gwenaël Casaccio <gwenael.casaccio@gmail.com>
2024-03-06 16:41:31 +0000

    chore: test should launch gc

--- libgst/object_table.cpp
+++ libgst/object_table.cpp
@@ -45,9 +45,27 @@ TEST_CASE("allocate different objects on the object table") {
 
   std::optional<std::reference_wrapper<object_s>>object = ot.alloc();
   REQUIRE_FALSE(object.has_value());
-  REQUIRE(ot.shouldLaunchGC());
 
   for (auto i = ot.begin(); i != ot.end(); i++) {
     REQUIRE((*i).getAllocatedFlag());
   }
 }
+
+TEST_CASE("should launch a GC when low on objects on the object table") {
+  ObjectTable<10> ot;
+
+  for (auto i = ot.begin(); i != ot.end(); i++) {
+    REQUIRE_FALSE((*i).getAllocatedFlag());
+  }
+
+  for (uint8_t i = 0; i < 8; i++) {
+    std::optional<std::reference_wrapper<object_s>>object = ot.alloc();
+    REQUIRE(object.has_value());
+    REQUIRE_FALSE(ot.shouldLaunchGC());
+  }
+
+  std::optional<std::reference_wrapper<object_s>>object = ot.alloc();
+  REQUIRE(object.has_value());
+  REQUIRE(ot.shouldLaunchGC());
+}
+
